3a4faa3fcfb362118c7b0eb9ef0bc6266bd1bfd4
coldflame426 <james.lee0432@gmail.com>
2021-10-04 10:16:47 -0400

    Complete mobile

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {
+  Image,
+  Linking,
+  Platform,
   SafeAreaView,
   StatusBar,
   StyleSheet,
@@ -11,20 +14,38 @@ import {
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
+const dAppLink = 'https://metamask.app.link/dapp/stormx-test.surge.sh/';
+
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() !== 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
     flex: 1,
   };
 
+  const handlePress = async () => {
+    const supported = await Linking.canOpenURL(dAppLink);
+    if (supported) {
+      await Linking.openURL(dAppLink);
+    } else {
+      if (Platform.OS === 'android') {
+        Linking.openURL('market://details?id=io.metamask');
+      } else if (Platform.OS === 'ios') {
+        Linking.openURL(
+          'itms-apps://apps.apple.com/us/app/metamask-blockchain-wallet/id1438144202',
+        );
+      }
+    }
+  };
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View style={styles.wrapper}>
-        <TouchableOpacity style={styles.connectWallet}>
-          <Text>Connect Wallet</Text>
+        <Image style={styles.logo} source={require('./assets/img/logo.png')} />
+        <TouchableOpacity style={styles.connectWallet} onPress={handlePress}>
+          <Text style={styles.connectText}>Open Dapp</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
@@ -35,13 +56,20 @@ const styles = StyleSheet.create({
   wrapper: {
     display: 'flex',
     flex: 1,
+    justifyContent: 'space-around',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  logo: {
+    resizeMode: 'contain',
+    width: 200,
   },
   connectWallet: {
-    backgroundColor: Colors.lighter,
-    padding: 15,
+    backgroundColor: Colors.darker,
     borderRadius: 10,
+    padding: 15,
+  },
+  connectText: {
+    color: Colors.lighter,
   },
 });
 
